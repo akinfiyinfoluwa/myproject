b54f4f91172598d0e1094361a20cf2d4b8ca744b
Emmanuel Akin-Ademola <75612661+akinfiyinfoluwa@users.noreply.github.com>
2024-06-22 20:27:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-function toggleNavbar() {
-    var navbarCollapse = document.getElementById("navbar-collapse");
-    navbarCollapse.classList.toggle("active");
-  
-    // Add the following lines
-    var body = document.body;
-    body.style.overflow = navbarCollapse.classList.contains("active") ? "hidden" : "auto";
-  }
+function handleClick() {
+  document.body.classList.toggle('dark-mode');
+}
+
+function handleClicker() {
+  document.body.classList.toggle('destroy');
+}
